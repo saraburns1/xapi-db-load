441516f1248bd911f48c8b1d95891d116831cb31
Sara Burns <sburns@axim.org>
2024-09-19 15:13:32 -0400

    testing

--- xapi_db_load/backends/clickhouse_lake.py
+++ xapi_db_load/backends/clickhouse_lake.py
@@ -212,8 +212,8 @@ class XAPILakeClickhouse:
 
             out_profile.append(profile_row)
 
-        # self._insert_list_sql_retry(out_external_id, "external_id")
-        self._insert_list_sql_retry(out_profile, "user_profile")
+        self._insert_list_sql_retry(out_external_id, "external_id")
+        # self._insert_list_sql_retry(out_profile, "user_profile")
 
     def insert_event_sink_taxonomies(self, taxonomies):
         """
